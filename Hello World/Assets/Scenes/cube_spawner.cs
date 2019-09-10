8e5ce18bc034343761f51defad828ce9f2de9a64
etomasik <etomasik@student.sussex.edu>
2019-09-10 14:15:16 -0400

    Second commit

---
+++ Hello World/Assets/Scenes/cube_spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube_spawner : MonoBehaviour
+{
+    public GameObject cubeprefabVar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(cubeprefabVar);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiate(cubeprefabVar);
+    }
+}
