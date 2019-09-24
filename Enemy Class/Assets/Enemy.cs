3459298857667af0f9935e5969df030c5ab833ee
etomasik <etomasik@student.sussex.edu>
2019-09-24 13:53:07 -0400

    First commit

---
+++ Enemy Class/Assets/Enemy.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+
+{
+
+    public float speed = 10f;
+    public float firerate = 0.3f;
+
+    void Update()
+
+    {
+        Move();
+    }
+
+    public virtual world Move()
+
+    {
+        Vector3 temPos = pos;
+        temPos.y -= speed * Time.deltaTime;
+        pos = temPos;
+
+    }
+
+    void OnCollisonEnter( Collision coll)
+
+    {
+        GameObject other = coll.gameObject;
+        switch (other.tag)
+
+        {
+            case "Hero":
+                break;
+            case "HeroLaser":
+                Destroy(this.gameObject);
+                break;
+        }
+    }
+
+    public Vector3 pos
+
+    {
+        get
+
+        {
+            return (this.transform.position);
+        }
+
+        set
+
+        {
+            this.transform.position = value;
+        }
+
+    }
+
+}
---
+++ Enemy Class/Assets/EnemyZig.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyZig : Enemy
+{ 
+    {
+        public override void Move ()
+        {
+            Vector3 temPos = pos;
+            temPos.x = Mathf.Sin(Time.time * Mathf.PI * 2) * 4;
+            pos = temPos;
+            base.Move();
+        }
+    }
+}
