f53a69ac911c55f9eb5fcebd38515f8c9d14549c
etomasik <etomasik@student.sussex.edu>
2019-10-15 14:00:14 -0400

    Fith Commit

---
+++ Mission Demolition Prototype/Assets/FollowCam.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public GameObject POI;
+    [Header("Set Dynamically")]
+    public float    camZ;
+
+    private void Awake()
+    {
+        camZ = this.transform.position.z;
+    }
+
+    private void FixedUpdate()
+    {
+        if (POI == null) return;
+        Vector3 destination = POI.transform.position;
+        destination.z = camZ;
+        transform.position = destination;
+    }
+}
