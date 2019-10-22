c7224bc6c3b71c71b398127d9ee5e1b70049fce5
etomasik <etomasik@student.sussex.edu>
2019-10-22 15:53:40 -0400

    Fith Commit

--- Mission Demolition Prototype/Assets/_Scripts/Slingshot.cs
+++ Mission Demolition Prototype/Assets/_Scripts/Slingshot.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    static private Slingshot S;
+
     [Header("Set in Inspector")]
     public GameObject prefabProjectile;
     public float velocityMult = 8f;
 
-    [Header("Set DYnamically")]
+    [Header("Set Dynamically")]
     public GameObject   launchPoint;
     public Vector3 launchPos;
     public GameObject projectile;
     public bool aimingMode;
     private Rigidbody   projectileRigidbody;
 
+    static public Vector3 LAUNCH_POS
+    {
+        get
+        {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+
     private void Awake()
 
     {
+        S = this;
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
