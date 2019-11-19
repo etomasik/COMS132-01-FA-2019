0e06ab7edff2e9ce29217f89cc65ccca5db75cc9
etomasik <etomasik@student.sussex.edu>
2019-11-19 15:44:46 -0500

    Ninth Commit

--- Space SHMUP Prototype/Assets/__Scripts/BoundsCheck.cs
+++ Space SHMUP Prototype/Assets/__Scripts/BoundsCheck.cs
@@ -29,25 +29,21 @@ public class BoundsCheck : MonoBehaviour
         if (pos.x > camWidth - radius)
         {
             pos.x = camWidth - radius;
-            isOnScreen = false;
             offRight = true;
         }
         if (pos.x < -camWidth + radius)
         {
             pos.x = -camWidth + radius;
-            isOnScreen = false;
             offLeft = true;
         }
         if (pos.y > camHeight - radius)
         {
             pos.y = camHeight - radius;
-            isOnScreen = false;
             offUp = true;
         }
         if (pos.y < -camHeight + radius)
         {
             pos.y = -camHeight + radius;
-            isOnScreen = false;
             offDown = true;
         }
 
