2a884e02d80cd9d01e9c84cdee557db74e3db2e4
etomasik <etomasik@student.sussex.edu>
2019-11-26 15:36:40 -0500

    Eleventh Commit

---
+++ Space SHMUP Prototype Plus/Assets/__Scripts/Weapon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponType
+{
+    none,
+    blaster,
+    spread,
+    phaser,
+    missile,
+    laser,
+    shield
+}
+
+[System.Serializable]
+public class WeaponDefinition
+{
+    public WeaponType type = WeaponType.none;
+    public string letter;
+    public Color color = Color.white;
+    public GameObject projectilePrefab;
+    public Color projectileColor = Color.white;
+    public float damageOnHit = 0;
+    public float continousDamage = 0;
+    public float delayBetweenShots = 0;
+    public float velocity = 20;
+}
+
+public class Weapon : MonoBehaviour
+{
+
+}
